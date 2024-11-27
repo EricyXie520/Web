0d61289cacd201b9d98f855ec793ae0389756d0e
shaojiu <3046368778@qq.com>
2024-11-27 16:05:26 +0800

    Create Javascript基础.js

---
+++ Javascript基础.js
@@ -0,0 +1,14 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// <script type="text/Javascrip" src="引入js文件">
+// alert（'这是一个弹窗'）;
+// </script>
+// </head>
+// <body>
+    
+// </body>
+// </html>
